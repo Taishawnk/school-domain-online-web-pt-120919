5fafd4491e365655c453f5aa45289a9b7e0fa9fd
Taishawn King <taishawnking1@yahoo.com>
2020-01-05 23:02:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,16 +2,15 @@ require"pry"
 class School
   attr_accessor :name, :roster
  def initialize(name)
-   @name=name
-@roster={}
+  @name=name
+  @roster={}
+  # everthing in intialize needs to be a instance veriable
 #binding.pry
  end
  
 
-   
- 
  def add_student(student,grade)
-
+ @roster << add_student
 
  end
  
